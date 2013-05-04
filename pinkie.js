5d47bbbef94cdacd4b8d718945614cfd61cd41c5
adlq <tnghia.duong@gmail.com>
2013-05-04 23:56:34 +0200

    Avoid duplicate event handlers

--- pinkie.js
+++ pinkie.js
@@ -1,16 +1,22 @@
-(function(){
-  window.Pinkie = {
-    watch : function(type, callback){
-      document.addEventListener(type, callback, true);
-    },
+window.Pinkie = {
+  eventList : [],
+  watch : function(type, callback){
+    var key = type+callback;
+    if(key in this.eventList
+        && this.eventList[key] === 1){
+      return;
+    }
+    document.addEventListener(type, callback, true);
+    this.eventList[key] = 1;
+  },
 
-    fire : function(type){
-      document.dispatchEvent(new CustomEvent(
-          type, false, false));
-    },
+  fire : function(type){
+    document.dispatchEvent(new CustomEvent(
+        type, false, false));
+  },
 
-    unwatch : function(type, callback){
-      document.removeEventListener(type, callback, false);
-    },
-  };
-})();
+  unwatch : function(type, callback){
+    document.removeEventListener(type, callback, true);
+    this.eventList[type+callback] = 0;
+  },
+};
